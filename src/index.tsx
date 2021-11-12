8643ff77ed808ee49afd29531e2489dfd70a3ee2
melpc <s.efremov@mel.fm>
2021-11-12 11:28:28 +0300

    setted the sass preprocessor

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from 'react';
 import ReactDOM from 'react-dom';
 
+import './test.scss'
 interface propApp {
     today: string;
 }
 
-const App: FC<propApp> = ({ today }) => <h1>My React and TypeScript App!!{today}</h1>;
+const App: FC<propApp> = ({ today }) => <h1 className="text">My React and TypeScript App!!{today}</h1>;
 
 ReactDOM.render(
     <React.StrictMode>
